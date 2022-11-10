ba9db5d2a8601c472e39adb69a6985ec089b0393
mcdonaldduncan <380939@colum.edu>
2022-11-09 21:18:38 -0600

    update

--- MyFile.cs
+++ MyFile.cs
@@ -12,10 +12,6 @@ namespace Week9ETL
         public string FilePath { get; set; }
         public string Extension { get; set; }
 
-        public MyFile()
-        {
-        }
-
         public MyFile(string _delimiter, string _filePath, string extension)
         {
             Delimiter = _delimiter;
